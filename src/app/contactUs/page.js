9a9afbce8b452afdaadaf828fb74f671a9baa445
Rafi5734 <kamrulhasun5734@gmail.com>
2023-07-11 10:07:01 +0600

    contact us page design done

--- src/app/contactUs/page.js
+++ src/app/contactUs/page.js
@@ -104,78 +104,83 @@ export default function ContactUs() {
             <h3 className="border-bottom pb-2">Send us your Feedback !</h3>
             <Form noValidate validated={validated} onSubmit={handleSubmit}>
               <Row className="mb-3">
-                <Form.Group as={Col} md="4" controlId="validationCustom01">
-                  <Form.Label>First name</Form.Label>
+                <Form.Group as={Col} md="12" controlId="validationCustom01">
+                  <Form.Label>Full name</Form.Label>
                   <Form.Control
                     required
                     type="text"
-                    placeholder="First name"
-                    defaultValue="Mark"
+                    size="sm"
+                    placeholder="Full name"
+                    // defaultValue="Mark"
                   />
                   <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
-                </Form.Group>
-                <Form.Group as={Col} md="4" controlId="validationCustom02">
-                  <Form.Label>Last name</Form.Label>
-                  <Form.Control
-                    required
-                    type="text"
-                    placeholder="Last name"
-                    defaultValue="Otto"
-                  />
-                  <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
-                </Form.Group>
-                <Form.Group
-                  as={Col}
-                  md="4"
-                  controlId="validationCustomUsername"
-                >
-                  <Form.Label>Username</Form.Label>
-                  <InputGroup hasValidation>
-                    <InputGroup.Text id="inputGroupPrepend">@</InputGroup.Text>
-                    <Form.Control
-                      type="text"
-                      placeholder="Username"
-                      aria-describedby="inputGroupPrepend"
-                      required
-                    />
-                    <Form.Control.Feedback type="invalid">
-                      Please choose a username.
-                    </Form.Control.Feedback>
-                  </InputGroup>
+                  <Form.Control.Feedback type="invalid">
+                    Please choose a correct name.
+                  </Form.Control.Feedback>
                 </Form.Group>
               </Row>
               <Row className="mb-3">
-                <Form.Group as={Col} md="6" controlId="validationCustom03">
-                  <Form.Label>City</Form.Label>
-                  <Form.Control type="text" placeholder="City" required />
+                <Form.Group as={Col} md="12" controlId="validationCustom03">
+                  <Form.Label>Email</Form.Label>
+                  <Form.Control
+                    type="email"
+                    size="sm"
+                    placeholder="Enter your email"
+                    required
+                  />
+                  <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
                   <Form.Control.Feedback type="invalid">
-                    Please provide a valid city.
-                  </Form.Control.Feedback>
-                </Form.Group>
-                <Form.Group as={Col} md="3" controlId="validationCustom04">
-                  <Form.Label>State</Form.Label>
-                  <Form.Control type="text" placeholder="State" required />
-                  <Form.Control.Feedback type="invalid">
-                    Please provide a valid state.
-                  </Form.Control.Feedback>
-                </Form.Group>
-                <Form.Group as={Col} md="3" controlId="validationCustom05">
-                  <Form.Label>Zip</Form.Label>
-                  <Form.Control type="text" placeholder="Zip" required />
-                  <Form.Control.Feedback type="invalid">
-                    Please provide a valid zip.
+                    Please provide a valid email.
                   </Form.Control.Feedback>
                 </Form.Group>
               </Row>
-              <Form.Group className="mb-3">
-                <Form.Check
-                  required
-                  label="Agree to terms and conditions"
-                  feedback="You must agree before submitting."
-                  feedbackType="invalid"
-                />
-              </Form.Group>
-              <Button type="submit">Submit form</Button>
+              <Row className="mb-3">
+                <Form.Group as={Col} md="12" controlId="validationCustom03">
+                  <Form.Label>Mobile</Form.Label>
+                  <Form.Control
+                    type="number"
+                    size="sm"
+                    placeholder="Enter your mobile number"
+                    required
+                  />
+                  <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
+                  <Form.Control.Feedback type="invalid">
+                    Please provide a valid mobile number.
+                  </Form.Control.Feedback>
+                </Form.Group>
+              </Row>
+              <Row className="mb-3">
+                <Form.Group as={Col} md="12" controlId="validationCustom03">
+                  <Form.Label>Subject</Form.Label>
+                  <Form.Control
+                    type="text"
+                    size="sm"
+                    placeholder="Enter your subject"
+                    required
+                  />
+                  <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
+                  <Form.Control.Feedback type="invalid">
+                    Please provide a subject.
+                  </Form.Control.Feedback>
+                </Form.Group>
+              </Row>
+              <Row className="mb-3">
+                <Form.Group as={Col} md="12" controlId="validationCustom03">
+                  <Form.Label>Details</Form.Label>
+                  <Form.Control
+                    as="textarea"
+                    size="sm"
+                    placeholder="Leave in details"
+                    style={{ height: "100px" }}
+                  />
+                  <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
+                  <Form.Control.Feedback type="invalid">
+                    Please provide a subject.
+                  </Form.Control.Feedback>
+                </Form.Group>
+              </Row>
+
+              <Button type="submit">Send</Button>
             </Form>
           </Col>
         </Row>
